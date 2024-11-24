7aaabd82e467a230c2e74666268e3fef6a58997a
madshopy <madsopyan5@gmail.com>
2024-11-24 14:49:18 +0700

    main

---
+++ latihanbelisusu.py
@@ -0,0 +1,41 @@
+"""
+semua sintaks dasar pemrograman terdiri dari
+1. sekunsial    : langkah berurutan
+2. percabangan  : langkah lompat bila kondisi terpenuhi
+3. perulangan   : mengulang langkah yang sama berulang-ulang kali sampai kondisi terpenuhi
+
+"""
+
+#sekuensial
+print('ibu berkata "belikan ibu susu 1 botol dan apabila ada telur beli kan telur 6"')
+print('anak menjawab "ok" ')
+
+#percabangan
+susu = 150
+telur = 2000
+
+print("\nwelcome to toko sembako")
+print("_"*30)
+
+print(f"stok susu saat ini : {susu}")
+print(f"stok telur saat ini : {telur}")
+
+if susu > 0 :
+    print('anak : "apakah ada susu" ?')
+    print(f'penjual menjawab : "stok susu ada" {susu} botol ')
+    print("beli susu satu botol")
+else :
+    print("tidak membeli susu")
+
+if telur > 0 :
+    print('anak : "apakah ada telur" ?')
+    print(f'penjual menjawab : "stok telur ada {telur} butir"')  # f berfungsi untuk meletakan varibale dalam text print
+    print("beli telur 6 butir")
+else :
+    print(" hanya membeli susu")
+
+print("anak pulang ke rumah dan menyerahkan hasil belanjanya kepada si ibu")
+print("selesai")
+
+
+print()
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+"""
+ini adalah demo project pertama python ku
+
+"""
 a = "budi"
 b = 45000
+c = "hello world"
 
+print(c)
 print("ada seorang anak yang bernama",a,"membawa uang sebanyak",b)
