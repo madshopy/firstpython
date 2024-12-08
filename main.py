5af9717525f8e58469abf60c2d5746007ce79769
madshopy <madsopyan5@gmail.com>
2024-12-08 14:35:41 +0700

    run package update-gempa-indonesia (final course)

--- main.py
+++ main.py
@@ -2,9 +2,29 @@
 ini adalah demo project pertama python ku
 
 """
-a = "budi"
-b = 45000
-c = "hello world"
+# a = "budi"
+# b = 45000
+# c = "hello world"
+#
+# print(c)
+# print("ada seorang anak yang bernama",a,"membawa uang sebanyak",b)
 
-print(c)
-print("ada seorang anak yang bernama",a,"membawa uang sebanyak",b)
+import gempaterkini
+
+from gempaterkini.gempaUPDATE.__init__ import bencana, gempaTerkini
+
+
+
+
+# aplikasi gempa terkini folder utama (pembuatan secara prosedural)
+if __name__ == '__main__':
+    result = gempaterkini.exstrasi_data()
+    gempaterkini.tampilkan_data(result)
+
+
+## aplikasi gempa terkini subfolder (pembuatan secara OOP)
+if __name__ == '__main__':
+    gempa_indonesia = gempaTerkini("https://bmkg.go.id")
+    print(f'Aplikasi Utama menggunakan package yang memiliki deskripsi {gempa_indonesia.description}')
+    gempa_indonesia.tampilkan_keterangan()
+    gempa_indonesia.run()
